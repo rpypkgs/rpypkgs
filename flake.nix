baadc1bdd7319e68890f2fcfb489c5a3df0ca61b
Corbin <cds@corbinsimpson.com>
2024-02-05 19:02:38 -0800

    Add Brainfuck interpreter.

--- flake.nix
+++ flake.nix
@@ -22,8 +22,41 @@
             "python-2.7.18.6"
           ];
         };
+        pypySrc = pkgs.fetchFromGitHub {
+          owner = "pypy";
+          repo = "pypy";
+          rev = "1fca5847f1902f76523d805ed291763b23733ccb";
+          sha256 = "sha256-hKZ0KRY6cT4C/7boiBqtv28WjhAcVABuiqtJRsFNHDk=";
+        };
+        bf = pkgs.stdenv.mkDerivation {
+          pname = "bf";
+          version = "5";
+
+          src = pkgs.fetchFromGitHub {
+            owner = "MG-K";
+            repo = "pypy-tutorial-ko";
+            rev = "20dd2e807014c75b53d6ed152fe38cb7af171301";
+            sha256 = "sha256-7YINSBwuEsuPlCW9Euo0Rs/0Nc6z1n+6g+Wtk332fb4=";
+          };
+
+          buildPhase = ''
+            cp -r ${pypySrc}/{rpython,py} .
+            # mkdir -p rpython/_cache/
+            chmod -R u+w rpython/
+            ${pkgs.pypy2}/bin/pypy rpython/bin/rpython example5.py
+          '';
+
+          installPhase = ''
+            mkdir -p $out/bin/
+            cp example5-c $out/bin/bf
+
+            mkdir -p $out/share/
+            cp *.b $out/share/
+          '';
+        };
       in {
         packages = {
+          inherit bf;
           inherit (pkgs) pypy2 pypy27 pypy3 pypy38 pypy39;
           typhon = typhon.packages.${system}.typhonVm;
         };
