53bbbbf0216d0cb6b125c7f4e8fa5f1f5dd22fc7
Corbin <cds@corbinsimpson.com>
2024-02-05 18:24:50 -0800

    Add Typhon from a flake.

--- flake.nix
+++ flake.nix
@@ -4,14 +4,28 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/release-23.05";
     flake-utils.url = "github:numtide/flake-utils";
+    typhon = {
+      url = "github:monte-language/typhon";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, typhon }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; }; in
-      {
-        packages = rec {
-          inherit (pkgs) pypy2 pypy27 pypy3 pypy39 pypy310;
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.permittedInsecurePackages = [
+            "python-2.7.18.6"
+          ];
+        };
+      in {
+        packages = {
+          inherit (pkgs) pypy2 pypy27 pypy3 pypy38 pypy39;
+          typhon = typhon.packages.${system}.typhonVm;
         };
         devShells.default = pkgs.mkShell {
           packages = [];
